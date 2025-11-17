8a497f75e165a05b87474a09731445f5a988fb20
Theis <theisrud@gmail.com>
2025-11-17 10:23:21 +0100

    Added collection of films

--- src/app/FilmBase.java
+++ src/app/FilmBase.java
@@ -1,9 +1,31 @@
 package app;
 
+import data.Film;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class FilmBase {
+    private Collection<Film> allFilms = new ArrayList<>();
+
     public FilmBase() {
     }
+
     public void start(){
+        initFilms();
         System.out.println("FilmBasen er startet");
+    }
+
+    private void initFilms(){
+        allFilms.add(new Film("The Godfather", 1972));
+        allFilms.add(new Film("The Shawshank Redemption", 1994));
+        allFilms.add(new Film("Schindler's List", 1993));
+        allFilms.add(new Film("Raging Bull", 1980));
+        allFilms.add(new Film("Casablanca", 1942));
+        allFilms.add(new Film("Citizen Kane", 1941));
+        allFilms.add(new Film("Gone With The Wind", 1939));
+        allFilms.add(new Film("The Wizard Of Oz", 1939));
+        allFilms.add(new Film("One Flew Over The Cuckoo's Nest", 1975));
+        allFilms.add(new Film("Lawrence Of Arabia", 1962));
     }
 }
--- src/data/Film.java
+++ src/data/Film.java
@@ -1,4 +1,27 @@
 package data;
 
 public class Film {
+    String title;
+    int year;
+
+    public Film(String title, int year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getAge() {
+        return year;
+    }
+
+    public void setAge(int age) {
+        this.year = age;
+    }
 }
